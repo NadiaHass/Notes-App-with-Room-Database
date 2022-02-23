df916990529dcba402eee62ddbc2406e451cc1fa
Nadia Hassouni <hassouninadia20@gmail.com>
2022-02-23 20:08:28 +0100

    fragments

---
+++ app/src/main/java/com/nadiahassouni/notes/fragments/AddNoteFragment.kt
@@ -0,0 +1,11 @@
+package com.nadiahassouni.notes.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nadiahassouni.notes.R
+
+class AddNoteFragment : Fragment() {
+}
---
+++ app/src/main/java/com/nadiahassouni/notes/fragments/UpdateFragment.kt
@@ -0,0 +1,12 @@
+package com.nadiahassouni.notes.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nadiahassouni.notes.R
+
+class UpdateFragment : Fragment() {
+
+}
